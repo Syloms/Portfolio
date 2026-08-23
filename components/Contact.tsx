2d25f44867a4b84098a14393b2436bd5740db6c2
Sylom <saberu1213@gmail.com>
2026-08-23 21:06:29 +0800

    certifications: with images

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,6 @@
 const links = [
-  { label: "email", value: "you@example.com", href: "mailto:you@example.com" },
-  { label: "github", value: "github.com/yourusername", href: "https://github.com/yourusername" },
+  { label: "email", value: "you@example.com", href: "mailto:ethanjamesestino@gmail.com" },
+  { label: "github", value: "github.com/yourusername", href: "https://github.com/Sylom" },
   { label: "linkedin", value: "linkedin.com/in/yourname", href: "https://linkedin.com/in/yourname" },
   { label: "tryhackme", value: "tryhackme.com/p/yourname", href: "https://tryhackme.com/p/yourname" },
 ];
